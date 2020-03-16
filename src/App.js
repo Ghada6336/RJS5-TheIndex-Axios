379aaaf78fb35c2f621c13ce974dc45f3f5f4969
Ghada6336 <ghadafahmie6336@gmail.com>
2020-03-16 17:49:36 +0200

    task5

--- src/App.js
+++ src/App.js
@@ -1,64 +1,67 @@
 import React, { Component } from "react";
-import axios from "axios";
+
+
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
-import Loading from "./Loading";
 import AuthorDetail from "./AuthorDetail";
-
+import Loading from "./Loading";
+import axios from "axios";
 class App extends Component {
   state = {
     currentAuthor: null,
-    authors: [],
-    loading: true
-  };
+    authors:[] ,
+    loading: true,
 
-  async componentDidMount() {
-    try {
-      const res = await axios.get(
-        "https://the-index-api.herokuapp.com/api/authors/"
-      );
-      const authors = res.data;
-      this.setState({
-        authors: authors,
-        loading: false,
-      });
-    } catch (err) {
-      console.error(err);
-    }
+  };
+async componentDidMount(){
+  try{
+    const getting= await axios.get('https://the-index-api.herokuapp.com/api/authors/');
+    const authors=getting.data;
+    this.setState({
+      authors:authors ,
+    loading: false,
+    });
+  
+  }
+  catch(t){
+    console.error(t);
+
   }
 
-  selectAuthor = async authorID => {
-    this.setState({ loading: true });
+
+};
+ 
+  selectAuthor = async author => {
+    
     try {
-      const res = await axios.get(
-        `https://the-index-api.herokuapp.com/api/authors/${authorID}/`
+      this.setState({ loading :true });
+    const getting= await axios.get(
+      `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
       );
-      const author = res.data;
-      this.setState({
-        currentAuthor: author,
-        loading: false,
-      });
-    } catch (err) {
-      console.error(err);
+    
+    this.setState({
+      currentAuthor:getting.data,
+      loading: false,
+    });
     }
-  };
+    catch(error){
+    console.error(error);
+    }
+  
+};
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   getContentView = () => {
-    if (this.state.loading) {
-      return <Loading />;
-    } else if (this.state.currentAuthor) {
+     if  (this.state.loading) {
+      return <Loading/>;
+    }
+    else if  (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return (
-        <AuthorList
-          authors={this.state.authors}
-          selectAuthor={this.selectAuthor}
-        />
-      );
+      return <AuthorList authors={this.state.authors} selectAuthor={this.selectAuthor} />;
     }
   };
 
